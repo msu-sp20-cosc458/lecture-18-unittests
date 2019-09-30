d66bd6812b56b6dc90990c15717ee8191ca184b9
Phil B <philanab@gmail.com>
2019-09-30 08:44:27 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,10 @@
+# tests.py
+import unittest # Do not forget this!
+
+class (unittest.TestCase): # Add your class name here.
+    def test_split_two_words(self):
+        result = .split() # Add your own two word string here.
+        # Add the test cases here.
+
+if __name__ == '__main__':
+    # Make sure you make the call to run the test.
