ef41f3b130e87d84dd9a0bad0bf68ca80965f72c
Phil B <philanab@gmail.com>
2019-09-30 09:19:06 -0400

    Update rand_functions.py

--- rand_functions.py
+++ rand_functions.py
@@ -1,10 +1,10 @@
 def foo_bar(n):
 	if n % 15 == 0:
-		return 'FizzBuzz'
+		return 'foo_bar'
 	elif n % 3 == 0:
-		return 'Fizz'
+		return 'foo'
 	elif n % 5 == 0:
-		return 'Buzz'
+		return 'bar'
 	else:
 		return str(n)
 
