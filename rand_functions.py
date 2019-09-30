4ddc411ed126317c4447d63a305263ea4bd6941f
Phil B <philanab@gmail.com>
2019-09-30 09:20:10 -0400

    Update rand_functions.py

--- rand_functions.py
+++ rand_functions.py
@@ -1,15 +1,15 @@
 def foo_bar(n):
 	if n % 15 == 0:
-		return 'foo_bar'
+		return "foo_bar"
 	elif n % 3 == 0:
-		return 'foo'
+		return "foo"
 	elif n % 5 == 0:
-		return 'bar'
+		return "bar"
 	else:
 		return str(n)
 
 def get_chatbot_response(message):
-    if message[:2] == '!!':
-        return 'WooHoo!'
+    if message[:2] == "!!":
+        return "WooHoo!"
     else:
-        return 'Invalid command'
+        return "Invalid command"
