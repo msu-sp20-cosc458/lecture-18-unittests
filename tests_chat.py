46e1f875ebd941bfc8fd5610b9474b56da849d38
Phil B <philanab@gmail.com>
2019-09-30 19:00:12 -0400

    Create tests_chat.py

---
+++ tests_chat.py
@@ -0,0 +1,15 @@
+# tests_chat.py
+import chat_bot
+import unittest
+
+class RandomFunctionsChatbotTestCase(unittest.TestCase):
+    def test_bangbang_means_command(self):
+        r = random_functions.get_chatbot_response('!!')
+        self.assertEquals(r, 'WooHoo')
+
+    def test_no_bangbang_means_no_command(self):
+        # here we need to create an invalid input
+        # we need to make sure our assertEquals function validates that our function handled the bad input correctly.
+
+if __name__ == '__main__':
+  # make sure we call the function that runs the unit tests!
